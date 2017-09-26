b8eb725bbf84a02c6e18745ef001bf0d85bc7b57
XJH0415 <31836788+XJH0415@users.noreply.github.com>
2017-09-26 10:27:56 +0800

    Update MainActivity.java

--- btsland/app/src/main/java/info/btsland/app/MainActivity.java
+++ btsland/app/src/main/java/info/btsland/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private ImageView ivNavUser ;
+    int i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
